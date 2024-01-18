f7a38d2b7afc96280b251735b3eb49916822b49f
Paulus Otto Harman <lonewolfffffff@gmail.com>
2024-01-19 03:21:19 +0700

    debug server port

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const http = require('http');
 http.createServer(function (req, res) {
   res.write('Halo Bro!');
   res.end();
-}).listen(8080);
+}).listen(3000, ()=> console.log('Server is running on port 3000'));
