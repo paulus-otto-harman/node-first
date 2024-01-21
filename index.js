18b5966d2e540a0e46f5fdeafae4005d1fe097a0
Paulus Otto Harman <lonewolfffffff@gmail.com>
2024-01-21 20:43:43 +0700

    handle favicon.ico request

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 http.createServer(function (req, res) {
-  res.write('Halo Bro!');
-  res.end();
+  if (req.url==='/favicon.ico') {
+    fs.readFile('./favicon.ico', function(err, data) {
+      if(err) {
+        res.writeHead(404);
+        res.write('Error : favicon.ico not found')
+      } else {
+        res.writeHead(200, {
+          "Content-Type": 'image/x-icon',
+        });
+        res.write(data);
+      }
+      res.end();
+    })
+  } else {
+    res.write("Halo Bro!");
+    res.end();
+  }
 }).listen(3000, ()=> console.log('Server is running on port 3000'));
